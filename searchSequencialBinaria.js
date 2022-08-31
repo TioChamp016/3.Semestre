ad235315ca7deb13195cfd69dbed9bfb28f84c88
TioChamp016 <gabrielveronezgiolo@gmail.com>
2022-08-30 22:33:55 -0300

    exercicio 5 resolvido agora

--- searchSequencialBinaria.js
+++ searchSequencialBinaria.js
@@ -1,3 +1,19 @@
 function principal(){
-    
+    //exercício 5
+    let n = Number(prompt(`Entre com o valor de "N"`))
+    resultado = calculando(n)
+    document.getElementById("saída").innerHTML = resultado
+    //calculando S
+}
+function calculando(n,resultado){
+    if(n <= 0){
+        return `Lamento. Mas "N" precisa ser um número positivo maior que zero para que o sistema consiga calcular o somatório S.`
+    }
+    else{
+        let s = 0
+        for(let i=1;i<=n;i++){
+            s = s + (i/(i**2))
+        }
+        return`O valor do somatório S é igual a: ${s}` 
+    }
 }
