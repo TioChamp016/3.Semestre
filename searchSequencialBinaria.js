a62c59fdb84a782fcc98d9083225bedf438fe017
TioChamp016 <gabrielveronezgiolo@gmail.com>
2022-08-31 16:14:51 -0300

    exercício 6 concluido + primeiras alterações no html

--- searchSequencialBinaria.js
+++ searchSequencialBinaria.js
@@ -1,13 +1,13 @@
-function principal(){
+function principal5(){
     //exercício 5
     let n = Number(prompt(`Entre com o valor de "N"`))
-    resultado = calculando(n)
+    resultado = calculando5(n)
     document.getElementById("saída").innerHTML = resultado
     //calculando S
 }
-function calculando(n,resultado){
+function calculando5(n){
     if(n <= 0){
-        return `Lamento. Mas "N" precisa ser um número positivo maior que zero para que o sistema consiga calcular o somatório S.`
+        return `Lamento. Mas "N" precisa ser um número inteiro positivo para que o sistema consiga calcular o somatório S.`
     }
     else{
         let s = 0
@@ -15,5 +15,33 @@ function calculando(n,resultado){
             s = s + (i/(i**2))
         }
         return`O valor do somatório S é igual a: ${s}` 
+    }
+}
+function principal6(){
+    //exercício 5
+    let n = Number(prompt(`Entre com o número "N" de elementos a preencher o vetor.`))
+    resultado = calculando6(n)
+    document.getElementById("saída").innerHTML = resultado
+    //calculando S
+}
+function calculando6(n){
+    let vet = []
+    let resultado = " "
+    if(n <= 0){
+        return `Lamento. Mas "N" precisa ser um número inteiro positivo para que o sistema consiga construir a sequencia de Fibonacci.`
+    }
+    else{
+        for(let i=0;i<(n);i++){
+            if (i==0 || i==1){
+                vet.push(1)
+                resultado += `${i+1}º Elemento: ${vet[i]}</br>`
+            }
+            else{
+                vet.push(vet[i-1]+vet[i-2])
+                resultado += `${i+1}º Elemento: ${vet[i]}</br>`
+            }
+        }
+        
+        return`A sequencia de Fibonacci com ${n} elementos é:</br>${resultado}` 
     }
 }
